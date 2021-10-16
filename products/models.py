f473ed651a5b21514425087b121f73cdea46c734
James Speller <J@miee.uk>
2021-10-16 01:43:33 +0100

    Added homepage, product upload and product details

--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    pubDate = models.DateTimeField()
+    body = models.TextField()
+    url = models.TextField()
+    image = models.ImageField(upload_to="images/")
+    icon = models.ImageField(upload_to="images/")
+    votesTotal = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+    def pubDateShort(self):
+        date = int(self.pubDate.strftime('%e'))
+        if 4 <= date <= 20 or 24 <= date <= 30:
+            suffix = "th "
+        else:
+            suffix = ["st ", "nd ", "rd "][date % 10 - 1]
+        properDate = self.pubDate.strftime('%B %e') + suffix + self.pubDate.strftime('%Y')
+        return properDate
